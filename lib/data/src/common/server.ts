611f245a1ebd06dafed694076616a86a87ecaa57
Sergio Vazquez <alb.vazq@gmail.com>
2024-06-03 12:44:13 -0600

    Validate input schemas in request handler

--- lib/data/src/common/server.ts
+++ lib/data/src/common/server.ts
@@ -1,6 +1,6 @@
 import { RouteConfig } from '@asteasolutions/zod-to-openapi';
 import { NextRequest, NextResponse } from 'next/server';
-import { ZodType, z } from 'zod';
+import { ZodBoolean, ZodNumber, ZodType, z } from 'zod';
 
 type NextBaseRequest<P, Q> = NextRequest & {
   nextUrl: { searchParams: { get: (key: keyof Q) => any } };
@@ -33,6 +33,39 @@ export const createRequestHandler = <
   const requestHandler = async (
     request: NextBaseRequest<z.infer<URLParams>, z.infer<QueryParams>>,
   ): Promise<NextResponse<z.infer<Response>>> => {
+    try {
+      if (_def.schemas?.queryParams) {
+        const queryParams = {};
+        const params = request.nextUrl.searchParams.keys();
+        for (const param of params) {
+          if (_def.schemas?.queryParams.shape[param] instanceof ZodNumber) {
+            queryParams[param] = Number(
+              request.nextUrl.searchParams.get(param),
+            );
+          } else if (
+            _def.schemas?.queryParams.shape[param] instanceof ZodBoolean
+          ) {
+            queryParams[param] = Boolean(
+              request.nextUrl.searchParams.get(param),
+            );
+          } else {
+            queryParams[param] = request.nextUrl.searchParams.get(param);
+          }
+        }
+        _def.schemas?.queryParams.parse(queryParams);
+      }
+
+      if (_def.schemas?.payload) {
+        const payload = await request.json();
+        _def.schemas?.payload.parse(payload);
+      }
+    } catch (e) {
+      return new NextResponse(JSON.stringify(e), {
+        status: 200,
+        headers: { 'content-type': 'application/json' },
+      });
+    }
+
     const handlerReturn = await _def.handler(request);
     return new NextResponse(
       JSON.stringify(
