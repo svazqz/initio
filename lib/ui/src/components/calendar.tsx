8212a0eec1e0d3b34400a96d0a46d3383d80e6b3
Sergio Vazquez <alb.vazq@gmail.com>
2024-07-29 17:37:44 -0600

    Add command component test

--- lib/ui/src/components/calendar.tsx
+++ lib/ui/src/components/calendar.tsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
 
 import { cn } from '@next-base/lib-ui/utils';
-import { buttonVariants } from './button';
+import { buttonVariants } from '@next-base/lib-ui/components/button';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
