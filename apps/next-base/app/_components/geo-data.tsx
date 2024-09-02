400079adcd91f364a186a47636fa0bdff927d05c
Sergio Vázquez <alb.vazq@gmail.com>
2024-09-02 17:39:42 -0500

    Improve prtotocol buffers support (#17)
    Co-authored-by: Sergio Vazquez <sergio@incotechsystems.com>

--- apps/next-base/app/_components/geo-data.tsx
+++ apps/next-base/app/_components/geo-data.tsx
@@ -1,33 +1,43 @@
 'use client';
-import { useCallback, useEffect, useState } from 'react';
+import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import { apiConsumer } from '../../../../lib/data/src/common/client';
 import { Button, Input } from '@next-base/lib-ui';
-import { postGeoData } from 'apps/next-base/data/geo';
-
-const getGeoDataConsumer = apiConsumer(postGeoData);
+import { postGeoData } from 'apps/next-base/data/geo/api';
 
 const GeoData = function Index() {
   const [latitude, setLatitude] = useState(19.3906594);
   const [longitude, setLongitude] = useState(-99.308425);
-  const { query, queryKey } = getGeoDataConsumer({
+  const { query, queryKey } = apiConsumer(postGeoData)({
     body: {
       latitude,
       longitude,
     },
   });
 
-  const onClick = useCallback(() => {
-    setLatitude(21.92081);
-    setLongitude(-102.33495);
-  }, []);
+  const onChangeLatitude = (ev: ChangeEvent<HTMLInputElement>) => {
+    setLatitude(Number(ev.target.value));
+  };
+
+  const onChangeLongitude = (ev: ChangeEvent<HTMLInputElement>) => {
+    setLongitude(Number(ev.target.value));
+  };
+
+  if (query.isLoading) return <>Loading...</>;
+
+  if (query.error) return <>Error</>;
 
   return (
     <>
-      <Input placeholder="latitude" value={latitude} />
-      <Input placeholder="longitude" value={longitude} />
-      <Button variant={'outline'} onClick={() => onClick()}>
-        Hello World!
-      </Button>
+      <Input
+        placeholder="latitude"
+        onChange={onChangeLatitude}
+        value={latitude}
+      />
+      <Input
+        placeholder="longitude"
+        onChange={onChangeLongitude}
+        value={longitude}
+      />
       <br />
       <textarea style={{ width: '100%', height: '40px' }}>
         {JSON.stringify(query.data)}
