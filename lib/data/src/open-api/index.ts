616f214647737751cb8215278cdf8101452c160d
Sergio Vazquez <sergio@incotechsystems.com>
2024-09-23 13:20:06 -0600

    Improve open api processor

--- lib/data/src/open-api/index.ts
+++ lib/data/src/open-api/index.ts
@@ -25,7 +25,6 @@ exec(command, (err, stdout, stderr) => {
   if (err) {
     // node couldn't execute the command
     throw new Error(JSON.stringify(err));
-    return;
   }
   try {
     Object.entries(apiDefinitions).forEach(([namespace, definitions]) => {
@@ -57,38 +56,3 @@ exec(command, (err, stdout, stderr) => {
     console.log(JSON.stringify(e));
   }
 });
-
-/*
-
-
-
-try {
-  Object.entries(apiDefinitions).forEach(([namespace, definitions]) => {
-    Object.entries(definitions as any).forEach(([key, def]) => {
-      registry.registerPath((def as any).apiConfig);
-    });
-  });
-
-  const generator = new OpenApiGeneratorV3(registry.definitions);
-
-  const result = generator.generateDocument({
-    info: {
-      title: '',
-      version: '1',
-    },
-    openapi: '3.0.0',
-  });
-
-  fs.writeFileSync(
-    `${__dirname}/../../out/openapi-docs.json`,
-    JSON.stringify(result, null, 2),
-    {
-      encoding: 'utf-8',
-    },
-  );
-
-  console.log(JSON.stringify(result, null, 2));
-} catch (e) {
-  console.log(JSON.stringify(e));
-}
-*/
