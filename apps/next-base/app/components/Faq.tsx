edc2a5417ea9e15fcd1dfafbb1959f5be8ea7e6c
Sergio Vazquez <sergio@incotechsystems.com>
2024-09-24 15:51:05 -0600

    Landing docs updated

--- apps/next-base/app/components/Faq.tsx
+++ apps/next-base/app/components/Faq.tsx
@@ -72,7 +72,7 @@ const Faq = () => {
           <a
             href="/api/docs"
             target="_blank"
-            className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+            className="inline-flex items-center justify-center p-2 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
           >
             <span className="w-full">/api/docs</span>
             <svg
@@ -90,12 +90,12 @@ const Faq = () => {
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
-          </a>
+          </a>{' '}
           in JSON format or using the SwaggerUI accessing to{' '}
           <a
             href="docs"
             target="_blank"
-            className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+            className="inline-flex items-center justify-center p-2 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
           >
             <span className="w-full">/docs</span>
             <svg
