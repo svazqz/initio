7384c09aa65af4150536aeb5a208d0ecc9a71b90
Sergio Vazquez <sergio@incotechsystems.com>
2024-09-23 21:17:37 -0600

    Correct Docs page background

--- apps/next-base/app/docs/Swagger.tsx
+++ apps/next-base/app/docs/Swagger.tsx
@@ -4,9 +4,9 @@ import SwaggerUI from 'swagger-ui-react';
 
 const Swagger = () => {
   return (
-    <>
+    <div className="absolute w-full h-full bg-white overflow-y-scroll">
       <SwaggerUI url="/api/docs" />
-    </>
+    </div>
   );
 };
 
