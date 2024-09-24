400189dbf1829dfb7b24d6718157241961b4450b
Sergio Vazquez <sergio@incotechsystems.com>
2024-09-24 15:49:43 -0600

    Landing docs updated

--- apps/next-base/app/components/Faq.tsx
+++ apps/next-base/app/components/Faq.tsx
@@ -68,8 +68,52 @@ const Faq = () => {
           </p>
           <br />
           This will create the OpenAPI JSON automatically (as shown in the code
-          block) and the documentation can be accessed either from '/api/docs'
-          in JSON format or using the SwaggerUI accessing to '/docs'
+          block) and the documentation can be accessed either from{' '}
+          <a
+            href="/api/docs"
+            target="_blank"
+            className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <span className="w-full">/api/docs</span>
+            <svg
+              className="w-4 h-4 ms-2 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+          in JSON format or using the SwaggerUI accessing to{' '}
+          <a
+            href="docs"
+            target="_blank"
+            className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <span className="w-full">/docs</span>
+            <svg
+              className="w-4 h-4 ms-2 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
         </div>
         <div className="lg:w-2/3">
           <AceEditor
